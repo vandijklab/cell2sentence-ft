36526ab0db14e0cf6e67e2f261f58628ec87a5aa
Syed Rizvi <syed.rizvi@yale.edu>
2023-09-15 10:13:41 -0400

    added cell type column to example preprocessing and arrow dataset conversion

--- create_cell_sentence_arrow_dataset.py
+++ create_cell_sentence_arrow_dataset.py
@@ -1,4 +1,7 @@
 import os
+
+import anndata
+import numpy as np
 from datasets import Dataset
 
 
@@ -7,7 +10,13 @@ def create_partition_arrow_ds(filename, data_split="train"):
     with open(filename) as fp:
         sentences_list = [line.rstrip() for line in fp]
     
-    arrow_dict = { "input_ids": sentences_list }
+    preprocessed_adata = anndata.read_h5ad(os.path.join(CURRENT_DIR, "preprocessed_adata.h5ad"))
+    dataset_split_sample_indices = np.load(os.path.join(CELL_SENTENCES_DIR, f"{data_split}_partition_indices.npy"))
+    dataset_split_preprocessed_adata = preprocessed_adata[dataset_split_sample_indices, :].copy()
+
+    cell_type_list = dataset_split_preprocessed_adata.obs["cell_type"].tolist()
+    
+    arrow_dict = { "input_ids": sentences_list, "cell_type": cell_type_list }
 
     arrow_ds = Dataset.from_dict(arrow_dict)
     arrow_ds.save_to_disk(os.path.join(OUTPUT_DIR, f"{data_split}_arrow_ds"))
@@ -21,8 +30,7 @@ def main():
 
 
 if __name__ == "__main__":
-    # CURRENT_DIR = os.getcwd()
-    CURRENT_DIR = "/home/sr2464/Desktop/cell2sentence-ft"
+    CURRENT_DIR = os.getcwd()
     CELL_SENTENCES_DIR = os.path.join(CURRENT_DIR, "cell_sentences")
     OUTPUT_DIR = os.path.join(CURRENT_DIR, "cell_sentence_arrow_ds")
     if not os.path.exists(OUTPUT_DIR):
